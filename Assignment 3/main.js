d3e2e0b5a4435231afcc3765c06b462a40ae77a4
Jadit19 <aditj20@iitk.ac.in>
2023-10-07 16:51:21 +0530

    add spheres

--- Assignment 3/main.js
+++ Assignment 3/main.js
@@ -61,8 +61,6 @@ uniform vec3 uSpecularLight;
 
 uniform samplerCube uEnv;
 
-uniform vec4 diffuseTerm;
-
 void main() {
   vec3 worldPosition = vWorldPosition;
   vec3 worldNormal = normalize(vWorldNormal);
@@ -94,6 +92,83 @@ void main() {
     1.0 );
   fragColor += vec4(texture(uEnv, normalize(reflect(-vectorView, worldNormal))).rgb, 0.0);
 }`
+const sphereVsCode = `#version 300 es
+in vec3 aNormal;
+in vec3 aPosition;
+
+uniform mat4 uMMatrix;
+uniform mat4 uPMatrix;
+uniform mat4 uVMatrix;
+
+out vec3 vModelPosition;
+out vec3 vModelNormal;
+out vec3 vWorldPosition;
+out vec3 vWorldNormal;
+
+void main() {
+  vModelPosition = aPosition;
+  vModelNormal = aNormal;
+
+  vec4 worldPosition = uMMatrix * vec4(aPosition, 1.0);
+
+  vWorldPosition = vec3(worldPosition);
+  vWorldNormal = normalize(mat3(uMMatrix) * aNormal);
+
+  gl_Position = uPMatrix * uVMatrix * worldPosition;
+}`
+const sphereFsCode = `#version 300 es
+precision mediump float;
+
+const float shininess = 1000.0;
+const float PI = 3.1415926535897932384626433832795;
+
+in vec3 vModelPosition;
+in vec3 vModelNormal;
+in vec3 vWorldPosition;
+in vec3 vWorldNormal;
+uniform vec3 vVertexColor;
+
+out vec4 fragColor;
+
+uniform vec3 uViewOrigin;
+uniform vec3 uLightDirection;
+uniform vec3 uAmbientLight;
+uniform vec3 uDiffuseLight;
+uniform vec3 uSpecularLight;
+
+uniform samplerCube uEnv;
+
+void main() {
+  vec3 worldPosition = vWorldPosition;
+  vec3 worldNormal = normalize(vWorldNormal);
+  vec3 modelPosition = vModelPosition;
+
+  modelPosition.y -= 1.0;
+  modelPosition = normalize(modelPosition);
+  vec3 modelNormal = normalize(vModelNormal);
+
+  vec2 textureCoord;
+  textureCoord.s = -atan(-modelPosition.z, -modelPosition.x) / 2.0 / PI + 0.5;
+  textureCoord.t = 0.5 - 0.5 * modelPosition.y;
+
+  vec3 tangentBAxis = vec3(0.0,1.0,0.0);
+  tangentBAxis = normalize(tangentBAxis - dot(tangentBAxis, worldNormal) * worldNormal);
+  vec3 tangentTAxis = normalize(cross(tangentBAxis, worldNormal));
+
+  vec3 normalizedLightDirection = normalize(uLightDirection);
+  vec3 vectorReflection = normalize(reflect(-normalizedLightDirection, worldNormal));
+  vec3 vectorView = normalize(uViewOrigin - worldPosition);
+
+  float diffuseLightWeighting = max( dot(worldNormal, normalizedLightDirection), 0.0 );
+  float specularLightWeighting = pow( max( dot(vectorReflection, vectorView), 0.0), shininess );
+
+  fragColor = vec4(
+    (8.0 * uAmbientLight * vVertexColor)
+    + ((uDiffuseLight * vVertexColor) * diffuseLightWeighting)
+    + ( uSpecularLight * specularLightWeighting),
+    1.0 );
+  fragColor += 0.4 * vec4(texture(uEnv, normalize(reflect(-vectorView, worldNormal))).rgb, 0.0);
+}`
 
 /** @type {Canvas} */
 var canvas
@@ -103,6 +178,9 @@ var skybox
 
 /** @type {Teapot} */
 var teapot
+
+/** @type {Sphere} */
+var sphere1, sphere2
 
 function degToRad (d) {
   return (d * Math.PI) / 180
@@ -456,10 +534,6 @@ class Teapot {
         this.shader.program,
         'uSpecularLight'
       ),
-      uDiffuseTerm: canvas.gl.getUniformLocation(
-        this.shader.program,
-        'diffuseTerm'
-      ),
       uEnv: canvas.gl.getUniformLocation(this.shader.program, 'uEnv')
     }
   }
@@ -534,7 +608,6 @@ class Teapot {
       0
     )
 
-    canvas.gl.uniform4fv(this.locations.uDiffuseTerm, [0.7, 0.2, 0.2, 1.0])
     canvas.gl.uniformMatrix4fv(this.locations.uMMatrix, false, this.mMatrix)
     canvas.gl.uniformMatrix4fv(this.locations.uVMatrix, false, mvMatrix)
     canvas.gl.uniformMatrix4fv(this.locations.uPMatrix, false, pMatrix)
@@ -550,6 +623,201 @@ class Teapot {
       canvas.gl.TRIANGLES,
       this.buffer.index.numItems,
       canvas.gl.UNSIGNED_SHORT,
+      0
+    )
+
+    canvas.gl.disableVertexAttribArray(this.locations.aPosition)
+    canvas.gl.disableVertexAttribArray(this.locations.aNormal)
+  }
+}
+
+class Sphere {
+  constructor (color, number) {
+    this.color = [color[0] / 255, color[1] / 255, color[2] / 255]
+    this.number = number
+
+    this.init()
+  }
+
+  init () {
+    this.initShape()
+    this.initShader()
+    this.initBuffer()
+    this.initMatrices()
+  }
+
+  initShape () {
+    this.positionArray = []
+    this.indexArray = []
+    this.normalArray = []
+
+    var nSlices = 50
+    var nStacks = 50
+    var radius = 1.0
+    for (var i = 0; i <= nSlices; i++) {
+      var angle = (i * Math.PI) / nSlices
+      var comp1 = Math.sin(angle)
+      var comp2 = Math.cos(angle)
+
+      for (var j = 0; j <= nStacks; j++) {
+        var phi = (j * 2 * Math.PI) / nStacks
+        var comp3 = Math.sin(phi)
+        var comp4 = Math.cos(phi)
+
+        var xcood = comp4 * comp1
+        var ycoord = comp2
+        var zcoord = comp3 * comp1
+
+        this.positionArray.push(
+          -radius * xcood,
+          -radius * ycoord,
+          -radius * zcoord
+        )
+        this.normalArray.push(xcood, ycoord, zcoord)
+      }
+    }
+
+    for (var i = 0; i < nSlices; i++) {
+      for (var j = 0; j < nStacks; j++) {
+        var id1 = i * (nStacks + 1) + j
+        var id2 = id1 + nStacks + 1
+
+        this.indexArray.push(id1, id2, id1 + 1)
+        this.indexArray.push(id2, id2 + 1, id1 + 1)
+      }
+    }
+  }
+
+  initShader () {
+    this.shader = new Shader(sphereVsCode, sphereFsCode)
+
+    this.locations = {
+      aPosition: canvas.gl.getAttribLocation(this.shader.program, 'aPosition'),
+      aNormal: canvas.gl.getAttribLocation(this.shader.program, 'aNormal'),
+      uMMatrix: canvas.gl.getUniformLocation(this.shader.program, 'uMMatrix'),
+      uVMatrix: canvas.gl.getUniformLocation(this.shader.program, 'uVMatrix'),
+      uPMatrix: canvas.gl.getUniformLocation(this.shader.program, 'uPMatrix'),
+      uViewOrigin: canvas.gl.getUniformLocation(
+        this.shader.program,
+        'uViewOrigin'
+      ),
+      uLightDirection: canvas.gl.getUniformLocation(
+        this.shader.program,
+        'uLightDirection'
+      ),
+      uAmbientLight: canvas.gl.getUniformLocation(
+        this.shader.program,
+        'uAmbientLight'
+      ),
+      uDiffuseLight: canvas.gl.getUniformLocation(
+        this.shader.program,
+        'uDiffuseLight'
+      ),
+      uSpecularLight: canvas.gl.getUniformLocation(
+        this.shader.program,
+        'uSpecularLight'
+      ),
+      uEnv: canvas.gl.getUniformLocation(this.shader.program, 'uEnv'),
+      vVertexColor: canvas.gl.getUniformLocation(
+        this.shader.program,
+        'vVertexColor'
+      )
+    }
+  }
+
+  initBuffer () {
+    this.buffer = {
+      position: canvas.gl.createBuffer(),
+      index: canvas.gl.createBuffer(),
+      normal: canvas.gl.createBuffer()
+    }
+
+    canvas.gl.bindBuffer(canvas.gl.ARRAY_BUFFER, this.buffer.position)
+    canvas.gl.bufferData(
+      canvas.gl.ARRAY_BUFFER,
+      new Float32Array(this.positionArray),
+      canvas.gl.STATIC_DRAW
+    )
+    this.buffer.position.itemSize = 3
+    this.buffer.position.numItems =
+      this.positionArray.length / this.buffer.position.itemSize
+
+    canvas.gl.bindBuffer(canvas.gl.ELEMENT_ARRAY_BUFFER, this.buffer.index)
+    canvas.gl.bufferData(
+      canvas.gl.ELEMENT_ARRAY_BUFFER,
+      new Uint32Array(this.indexArray),
+      canvas.gl.STATIC_DRAW
+    )
+    this.buffer.index.itemSize = 1
+    this.buffer.index.numItems = this.indexArray.length
+
+    canvas.gl.bindBuffer(canvas.gl.ARRAY_BUFFER, this.buffer.normal)
+    canvas.gl.bufferData(
+      canvas.gl.ARRAY_BUFFER,
+      new Float32Array(this.normalArray),
+      canvas.gl.STATIC_DRAW
+    )
+    this.buffer.normal.itemSize = 3
+    this.buffer.normal.numItems =
+      this.normalArray.length / this.buffer.normal.itemSize
+  }
+
+  initMatrices () {
+    this.mMatrix = mat4.create()
+    mat4.identity(this.mMatrix)
+
+    if (this.number === 1) {
+      this.mMatrix = mat4.translate(this.mMatrix, [0, -0.06, 0.35])
+      mat4.scale(this.mMatrix, [0.1, 0.1, 0.1])
+    } else {
+      this.mMatrix = mat4.translate(this.mMatrix, [0.2, -0.1, 0.2])
+      mat4.scale(this.mMatrix, [0.05, 0.05, 0.05])
+    }
+  }
+
+  draw () {
+    canvas.gl.useProgram(this.shader.program)
+
+    canvas.gl.bindBuffer(canvas.gl.ARRAY_BUFFER, this.buffer.position)
+    canvas.gl.enableVertexAttribArray(this.locations.aPosition)
+    canvas.gl.vertexAttribPointer(
+      this.locations.aPosition,
+      this.buffer.position.itemSize,
+      canvas.gl.FLOAT,
+      false,
+      0,
+      0
+    )
+
+    canvas.gl.bindBuffer(canvas.gl.ELEMENT_ARRAY_BUFFER, this.buffer.index)
+
+    canvas.gl.bindBuffer(canvas.gl.ARRAY_BUFFER, this.buffer.normal)
+    canvas.gl.enableVertexAttribArray(this.locations.aNormal)
+    canvas.gl.vertexAttribPointer(
+      this.locations.aNormal,
+      this.buffer.normal.itemSize,
+      canvas.gl.FLOAT,
+      false,
+      0,
+      0
+    )
+
+    canvas.gl.uniformMatrix4fv(this.locations.uMMatrix, false, this.mMatrix)
+    canvas.gl.uniformMatrix4fv(this.locations.uVMatrix, false, mvMatrix)
+    canvas.gl.uniformMatrix4fv(this.locations.uPMatrix, false, pMatrix)
+    canvas.gl.uniform3fv(this.locations.uViewOrigin, viewOrigin)
+    canvas.gl.uniform1i(this.locations.uEnv, 0)
+
+    canvas.gl.uniform3fv(this.locations.uLightDirection, lightDirection)
+    canvas.gl.uniform3fv(this.locations.uAmbientLight, ambientLight)
+    canvas.gl.uniform3fv(this.locations.uDiffuseLight, diffuseLight)
+    canvas.gl.uniform3fv(this.locations.uSpecularLight, specularLight)
+    canvas.gl.uniform3fv(this.locations.vVertexColor, this.color)
+
+    canvas.gl.drawElements(
+      canvas.gl.TRIANGLES,
+      this.buffer.index.numItems,
+      canvas.gl.UNSIGNED_INT,
       0
     )
 
@@ -582,6 +850,8 @@ const initialize = () => {
 
   skybox = new Skybox('assets/images/')
   teapot = new Teapot('assets/meshes/teapot.json')
+  sphere1 = new Sphere([32, 82, 64], 1)
+  sphere2 = new Sphere([52, 66, 125], 2)
 }
 
 const drawScene = () => {
@@ -591,6 +861,8 @@ const drawScene = () => {
   canvas.clear()
   skybox.draw()
   teapot.draw()
+  sphere1.draw()
+  sphere2.draw()
 }
 
 const webGLStart = () => {
