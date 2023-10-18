98a12e9527c8da26476f318017aa3b8236680201
Jadit19 <aditj20@iitk.ac.in>
2023-10-18 14:02:19 +0530

    add contrast and brightness functionalities

--- Assignment 4/main.js
+++ Assignment 4/main.js
@@ -16,27 +16,42 @@ precision mediump float;
 in vec2 vTexCoord;
 out vec4 fragColor;
 
+uniform float mode;
+uniform float coloring;
+uniform float brightness;
+uniform float contrast;
 uniform sampler2D uBackgroundSampler;
 uniform sampler2D uForegroundSampler;
 
 void main(){
-  vec4 colorBackground = texture(uBackgroundSampler, vTexCoord);
-  vec4 colorForeground = texture(uForegroundSampler, vTexCoord);
-  float af =  colorForeground.a;
-  vec4 blend = af * colorForeground + (1.0 - af) * colorBackground;
 
-  float gray = 0.2126*blend.r + 0.7152*blend.g + 0.0722*blend.b;
-  vec4 grayscalecolor = vec4(gray, gray, gray, blend.a);
+  vec4 postMode;
+  if (mode == 0.0) {
+    postMode = texture(uBackgroundSampler, vTexCoord);
+  } else if (mode == 1.0){
+    vec4 colorBackground = texture(uBackgroundSampler, vTexCoord);
+    vec4 colorForeground = texture(uForegroundSampler, vTexCoord);
+    float af =  colorForeground.a;
+    postMode = af * colorForeground + (1.0 - af) * colorBackground;
+  }
 
-  float sr = 0.393*blend.r + 0.769*blend.g + 0.189*blend.b;
-  float sg = 0.349*blend.r + 0.686*blend.g + 0.168*blend.b;
-  float sb = 0.272*blend.r + 0.534*blend.g + 0.131*blend.b;
-  vec4 sepia = vec4(sr,sg,sb,blend.a);
+  vec4 postColoring;
+  if (coloring == 0.0) {
+    postColoring = postMode;
+  } else if (coloring == 1.0){
+    float gray = 0.2126*postMode.r + 0.7152*postMode.g + 0.0722*postMode.b;
+    postColoring = vec4(gray, gray, gray, postMode.a);
+  } else if (coloring == 2.0) {
+    float sr = 0.393*postMode.r + 0.769*postMode.g + 0.189*postMode.b;
+    float sg = 0.349*postMode.r + 0.686*postMode.g + 0.168*postMode.b;
+    float sb = 0.272*postMode.r + 0.534*postMode.g + 0.131*postMode.b;
+    postColoring = vec4(sr,sg,sb,postMode.a);
+  }
 
-  
-  // fragColor =  blend;
-  // fragColor = sepia;
-  fragColor = grayscalecolor;
+  vec4 postContrast = vec4(0.5 + (contrast+1.0)*(postColoring.rgb - 0.5), postColoring.a);
+  vec4 postBrightness = clamp(vec4(postContrast.rgb + brightness, postContrast.a), 0.0, 1.0);
+
+  fragColor = postBrightness;
 }`
 
 /** @type {Canvas} */
@@ -139,7 +154,11 @@ class Shader {
       uForegroundSampler: canvas.gl.getUniformLocation(
         this.program,
         'uForegroundSampler'
-      )
+      ),
+      mode: canvas.gl.getUniformLocation(this.program, 'mode'),
+      coloring: canvas.gl.getUniformLocation(this.program, 'coloring'),
+      brightness: canvas.gl.getUniformLocation(this.program, 'brightness'),
+      contrast: canvas.gl.getUniformLocation(this.program, 'contrast')
     }
   }
 }
@@ -194,7 +213,7 @@ class Inputs {
   }
 
   setupMode () {
-    this.mode = 0
+    this.mode = 1.0
     document.getElementById('mode-bg-only').addEventListener('change', e => {
       this.mode = 0.0
       drawScene()
@@ -223,7 +242,7 @@ class Inputs {
 
   setupContrast () {
     this.contrast = 0
-    document.getElementById('contrast').addEventListener('change', e => {
+    document.getElementById('contrast').addEventListener('input', e => {
       this.contrast = e.target.value
       drawScene()
     })
@@ -231,7 +250,7 @@ class Inputs {
 
   setupBrightness () {
     this.brightness = 0
-    document.getElementById('brightness').addEventListener('change', e => {
+    document.getElementById('brightness').addEventListener('input', e => {
       this.brightness = e.target.value
       drawScene()
     })
@@ -432,6 +451,10 @@ const drawScene = () => {
     shader.locations.uForegroundSampler,
     foregroundTexture.number
   )
+  canvas.gl.uniform1f(shader.locations.mode, inputs.mode)
+  canvas.gl.uniform1f(shader.locations.coloring, inputs.color)
+  canvas.gl.uniform1f(shader.locations.brightness, inputs.brightness)
+  canvas.gl.uniform1f(shader.locations.contrast, inputs.contrast)
 
   canvas.gl.drawElements(canvas.gl.TRIANGLES, 6, canvas.gl.UNSIGNED_SHORT, 0)
 }
