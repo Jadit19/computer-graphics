dfefad9636ca7d7e73ee151a6ecc5003c8abf236
Jadit19 <aditj20@iitk.ac.in>
2023-11-14 20:40:07 +0530

    ho gyaaaaaaaaaa ass 5

--- Assignment 5/main.js
+++ Assignment 5/main.js
@@ -8,234 +8,216 @@ const shaderCode = {
     }
   `,
   fragment: `#version 300 es
-    #define NUM_SPHERES 4
-    #define INFINITY 100000.0
 
-    precision mediump float;
-
-    uniform float uLight;
-    uniform int uMode;
-    uniform int uBounces;
-
-    vec3 initialLightDir;
-
-    out vec4 fragColor;
-
-    struct Sphere {
-      vec3 center;
-      float radius;
-      vec3 color;
-    };
-    
-    struct Ray {
-      vec3 origin;
-      vec3 direction;
-    };
-
-    Sphere spheres[NUM_SPHERES] = Sphere[NUM_SPHERES](
-      Sphere(vec3(-1.8, 0, -2), 1.2, vec3(0.0, 1.0, 0.0)),  // Sphere 1
-      Sphere(vec3(0.0, 0.5, -5), 2.3, vec3(1.0, 0.0, 0.0)),   // Sphere 2
-      Sphere(vec3(1.8, 0, -2), 1.3, vec3(0.0, 0.0, 1.0)),      // Sphere 3
-      Sphere(vec3(0, -21.5, 2), 20.0, vec3(1.0, 1.0, 1.0))    // Sphere 4
-    );
-
-    float shine[4] = float[4](20.0, 10.0, 50.0, 100.0);
-
-    // Function to solve the quadratic equation
-    bool solveQuadratic(float a, float b, float c, out float t0, out float t1) {
-      float disc = b*b - 4.*a*c;
-      if (disc < 0.0){
-        return false;
-      } else if (disc == 0.0){
-        t0 = t1 = -0.5 * b / a;
-        return true;
-      }
-
-      t0 = (-b + sqrt(disc)) / (2. * a);
-      t1 = (-b - sqrt(disc)) / (2. * a);
+  #define NUM_SPHERES 4
+  #define ROOT_3 1.73205080757
+  #define INFINITY 100000.0
+  
+  precision mediump float;
+  
+  uniform float uLight;
+  uniform int uMode;
+  uniform int uBounces;
+  
+  vec3 initialLightDir;
+  
+  out vec4 fragColor;
+  
+  struct Sphere {
+    vec3 center;
+    float radius;
+    vec3 color;
+    float shine;
+  };
+  
+  struct Ray {
+    vec3 origin;
+    vec3 direction;
+  };
+  
+  Sphere spheres[NUM_SPHERES] = Sphere[NUM_SPHERES](
+      Sphere(vec3(-ROOT_3, 0, -1), 0.8f, vec3(0.0f, 1.0f, 0.0f),
+             20.0),                                                    // Sphere 1
+      Sphere(vec3(0, 0, -2), 1.0f, vec3(1.0f, 0.0f, 0.0f), 10.0),      // Sphere 2
+      Sphere(vec3(ROOT_3, 0, -1), 0.8f, vec3(0.0f, 0.0f, 1.0f), 50.0), // Sphere 3
+      Sphere(vec3(0, -10, 0), 9.0f, vec3(1.0f, 1.0f, 1.0f), 100.0)     // Sphere 4
+  );
+  
+  // Function to solve the quadratic equation
+  bool solveQuadratic(float a, float b, float c, out float t0, out float t1) {
+    float disc = b * b - 4. * a * c;
+    if (disc < 0.0) {
+      return false;
+    } else if (disc == 0.0) {
+      t0 = t1 = -0.5 * b / a;
       return true;
     }
-
-    // Function to perform ray-sphere intersection
-    float trace(Ray ray, Sphere sphere, out vec3 normal) {
-      float a = dot(ray.direction,ray.direction);
-      float b = 2.0 * dot(ray.direction, ray.origin - sphere.center);
-      float c = dot(ray.origin - sphere.center, ray.origin - sphere.center) - sphere.radius * sphere.radius;
-      float t0, t1;
-
-      if (!solveQuadratic(a, b, c, t0, t1)) {
+  
+    t0 = (-b + sqrt(disc)) / (2. * a);
+    t1 = (-b - sqrt(disc)) / (2. * a);
+    return true;
+  }
+  
+  // Function to calculate lighting for phong shading
+  vec3 calcLighting(vec3 normal, vec3 viewDir, vec3 lightDir, vec3 objectColor,
+                    float shininess) {
+    vec3 ambient = 0.6 * objectColor;
+    vec3 diffuse = 0.2 * objectColor * max(dot(normal, lightDir), 0.0);
+    vec3 reflectDir = reflect(-lightDir, normal);
+    vec3 specular = 0.6 * vec3(1.0, 1.0, 1.0) *
+                    pow(max(dot(viewDir, reflectDir), 0.0), shininess);
+  
+    if (objectColor == vec3(1.0)) {
+      return (ambient + diffuse);
+    }
+    return (ambient + diffuse + specular);
+  }
+  
+  // Function to perform ray-sphere intersection
+  float trace(Ray ray, Sphere sphere, out vec3 normal) {
+    float a = dot(ray.direction, ray.direction);
+    float b = 2.0 * dot(ray.direction, ray.origin - sphere.center);
+    float c = dot(ray.origin - sphere.center, ray.origin - sphere.center) -
+              sphere.radius * sphere.radius;
+    float t0, t1;
+  
+    if (!solveQuadratic(a, b, c, t0, t1)) {
+      return -1.0;
+    }
+    if (t0 > t1) {
+      float temp = t0;
+      t0 = t1;
+      t1 = temp;
+    }
+    if (t0 < 0.0) {
+      t0 = t1;
+      if (t0 < 0.0) {
         return -1.0;
       }
-      if (t0 > t1) {
-        float temp = t0;
-        t0 = t1;
-        t1 = temp;
-      }
-      if (t0 < 0.0) {
-        t0 = t1;
-        if (t0 < 0.0) {
-          return -1.0;
-        }
-      }
-
-      float t = t0;
-      vec3 hitPoint = ray.origin + ray.direction * t;
-      normal = normalize(hitPoint - sphere.center);
-      return t;
     }
-
-    // Function to calculate lighting for phong shading
-    vec3 calcLighting(vec3 normal, vec3 viewDir, vec3 lightDir, vec3 objectColor, float shininess) {
-      vec3 ambient = 0.4 * objectColor;
-      vec3 diffuse = 0.2 * objectColor * max(dot(normal, lightDir), 0.0);
-      vec3 reflectDir = reflect(-lightDir, normal);
-      vec3 specular = 0.5 * vec3(1.0, 1.0, 1.0) * pow(max(dot(viewDir, reflectDir), 0.0), shininess);
-      return (ambient + diffuse + specular);
-    }
-
-    // Check whether the object is in shadow
-    bool inShadow(vec3 hitPoint, vec3 lightDir) {
-      vec3 normal;
-      for (int i=0; i < NUM_SPHERES; i++) {
-        float t = trace(Ray(hitPoint, lightDir), spheres[i], normal);
-        if (t > 0.1) {
-          return true;
-        }
+  
+    float t = t0;
+    vec3 hitPoint = ray.origin + ray.direction * t;
+    normal = normalize(hitPoint - sphere.center);
+    return t;
+  }
+  
+  bool inShadow(vec3 intersectionPoint, vec3 lightDir) {
+    vec3 normal;
+    for (int i = 0; i < 4; ++i) {
+      float t = trace(Ray(intersectionPoint, lightDir), spheres[i], normal);
+      if (t > 0.1) {
+        return true; // Point is in shadow
       }
-      return false;
     }
-
-    // Function to perform reflection
-    void reflection(Ray ray, vec3 normal, int depth, out vec3 reflectedColor) {
-      // vec3 reflectedDir = reflect(ray.direction, normal);
-      // vec3 reflectedDir = 2.0 * dot(-ray.direction, normal) * normal + ray.direction;
-      vec3 reflectedDir = ray.direction;
-      Ray reflectedRay = ray;
-
-      vec3 finalColor = vec3(0.0);
-      vec3 currentColor = vec3(1.0);
-
-      for (int i=0; i<uBounces; i++){
-        float closestIntersection = -1.0;
-        vec3 closestNormal = vec3(0.0);
-        int hitSphereIndex = -1;
-
-        // Find the closest intersection for the reflected ray
-        for (int j=0; j<NUM_SPHERES; j++){
-          float t = trace(reflectedRay, spheres[j], closestNormal);
-          if (t > 0.01 && (closestIntersection < 0.0 || t < closestIntersection)) {
-            closestIntersection = t;
-            closestNormal = closestNormal;
-            hitSphereIndex = j;
-          }
-        }
-
-        // If there's no intersection, set background color
-        if (closestIntersection < 0.0){
-          reflectedColor = finalColor;
-          return;
-        } else {
-          vec3 objectColor = spheres[hitSphereIndex].color;
-          float shininess = shine[hitSphereIndex];
-
-          // Calculate lighting
-          vec3 viewDir = normalize(-reflectedRay.direction);
-          vec3 point = reflectedRay.origin + closestIntersection * reflectedRay.direction;
-          vec3 lightDir = normalize(initialLightDir - point);
-          point = point - 0.001 * closestNormal;
-
-          // Calculate final color
-          vec3 phongColor = calcLighting(closestNormal, viewDir, lightDir, objectColor, shininess);
-          finalColor += currentColor * phongColor;
-
-          // Update the reflected ray for the next iteration
-          reflectedRay.origin = point - 0.01*closestNormal;
-          reflectedRay.direction = reflect(reflectedRay.direction, closestNormal);
-
-          // Update current color with object color for the next iteration
-          currentColor += objectColor;
-          currentColor = normalize(currentColor);
-        }
+    return false; // Point is not in shadow
+  }
+  
+  vec3 calculateNormal(vec3 intersectionPoint, int sphereIndex) {
+    return normalize(intersectionPoint - spheres[sphereIndex].center);
+  }
+  
+  Ray calculateColor(Ray ray, int depth, out vec3 outputColor) {
+    if (depth <= 0) {
+      outputColor = vec3(0.0); // Terminate recursion at a certain depth
+      return ray;
+    }
+  
+    float closestIntersection = INFINITY;
+    int closestSphereIndex = -1;
+    vec3 normal;
+    vec3 closestNormal;
+  
+    // Find the closest intersection
+    for (int i = 0; i < NUM_SPHERES; ++i) {
+      float t = trace(ray, spheres[i], normal);
+      if (t > 0.0 && (closestIntersection < 1.0 || t < closestIntersection)) {
+        closestIntersection = t;
+        closestNormal = normal;
       }
-
-      reflectedColor = finalColor;
     }
-
-    // Function to shade a pixel
-    vec4 shade(Ray ray){
-      vec3 normal;
-      vec3 finalColor = vec3(0.0);
-
-      float closestIntersection = -1.0;
-      vec3 closestNormal = vec3(0.0);
-
-      // Find the closest intersection
-      for (int i = 0; i < NUM_SPHERES; ++i) {
-        float t = trace(ray, spheres[i], normal);
-        if (t > 0.0 && (closestIntersection < 0.0 || t < closestIntersection)) {
+  
+    closestIntersection = INFINITY;
+    for (int i = 0; i < NUM_SPHERES; ++i) {
+      Sphere currentSphere = spheres[i];
+  
+      vec3 oc = ray.origin - currentSphere.center;
+      float a = dot(ray.direction, ray.direction);
+      float b = 2.0 * dot(oc, ray.direction);
+      float c = dot(oc, oc) - currentSphere.radius * currentSphere.radius;
+      float discriminant = b * b - 4.0 * a * c;
+  
+      if (discriminant >= 0.0) {
+        float t = (-b - sqrt(discriminant)) / (2.0 * a);
+  
+        if (t > 0.001 && t < closestIntersection) {
           closestIntersection = t;
-          closestNormal = normal; 
+          closestSphereIndex = i;
         }
       }
+    }
+  
+    if (closestSphereIndex == -1) {
+      outputColor = vec3(0.0); // No intersection, return background color
+      return ray;
+    }
+  
+    // Calculate intersection point  
 
-      // If no intersection is there, set the background color
-      if (closestIntersection < 0.0){
-        return vec4(0.0, 0.0, 0.0, 1.0);
+    vec3 intersectionPoint = ray.origin + ray.direction * closestIntersection;
+    vec3 otherNormal = calculateNormal(intersectionPoint, closestSphereIndex);
+  
+    vec3 color = spheres[closestSphereIndex].color;
+    float shininess = spheres[closestSphereIndex].shine;
+    vec3 viewDir = normalize(-ray.direction);
+    vec3 point = ray.origin + closestIntersection * ray.direction;
+    vec3 lightDir = normalize(initialLightDir - point);
+    vec3 neworigin = point + 0.0002 * normal;
+    bool shadowed = inShadow(neworigin, lightDir);
+  
+    vec3 phongColor =
+        calcLighting(closestNormal, viewDir, lightDir, color, shininess);
+    if (shadowed && depth == uBounces && closestSphereIndex == 3) {
+      if (uMode == 1 || uMode == 3){
+        phongColor -= vec3(0.2, 0.2, 0.2);
       }
-
-      vec3 objectColor = spheres[0].color;
-      float shininess = shine[0];
-
-      // Find the color of the intersected sphere
-      for (int i=0; i<NUM_SPHERES; i++){
-        if (closestIntersection == trace(ray, spheres[i], normal)) {
-          objectColor = spheres[i].color;
-          shininess = shine[i];
-          break;
+    }
+  
+    Ray reflectedRay;
+    reflectedRay.origin = intersectionPoint;
+    reflectedRay.direction = reflect(ray.direction, otherNormal);
+  
+    outputColor = phongColor;
+    return reflectedRay;
+  }
+  
+  void main() {
+    vec2 screenCoords = (gl_FragCoord.xy / vec2(500, 500)) * 2.0f - 1.0f;
+    initialLightDir = vec3(uLight, 5.0f, -3.0f);
+  
+    vec3 rayDirection = normalize(vec3(screenCoords, -1.0f));
+    vec3 rayOrigin = vec3(0.0f, 0.1f, 1.5);
+    Ray ray = Ray(rayOrigin, rayDirection);
+  
+    // Perform ray tracing with a loop for bounces
+    vec3 tempColor;
+    vec3 finalColor;
+    ray = calculateColor(ray, uBounces, finalColor);
+    // finalColor = normalize(finalColor);
+  
+    if (uMode > 1) {
+      for (int bounce = uBounces - 1; bounce > 0; --bounce) {
+        ray = calculateColor(ray, bounce, tempColor);
+        // finalColor = normalize(0.2*finalColor + 0.6*tempColor);
+        // finalColor = normalize(finalColor);
+        if (tempColor != vec3(0.0)) {
+          finalColor = 0.6 * finalColor + 0.4 * tempColor;
         }
       }
-
-      vec3 viewDir = normalize(-ray.direction);
-      vec3 point = ray.origin + closestIntersection * ray.direction;
-      vec3 lightDir = normalize(initialLightDir - point);
-      vec3 newOrigin = point + 0.0002*normal;
-
-      // Check if the new origin is in shadow or not
-      bool isShadowed = inShadow(newOrigin, lightDir);
-
-      if (isShadowed && (uMode == 1 || uMode == 3)) {
-        return vec4(0.2, 0.2, 0.2, 1.0);
-      }
-
-      vec3 phongColor = calcLighting(closestNormal, viewDir, lightDir, objectColor, shininess);
-
-      // Adding environment reflection using ray tracing
-      vec3 reflectedDir = reflect(ray.direction, closestNormal);
-      Ray reflectedRay = Ray(newOrigin, reflectedDir);
-      vec3 reflectedColor = vec3(0.0);
-
-      // Making reflection
-      reflection(reflectedRay, closestNormal, 0, reflectedColor);
-
-      if (uMode > 1){
-        phongColor += 0.8 * reflectedColor;
-      }
-
-      finalColor = phongColor;
-      return vec4(finalColor, 1.0);
     }
-
-    void main() {
-      vec2 screenCoords = (gl_FragCoord.xy / vec2(800, 800)) * 2.0 - 1.0;
-      initialLightDir = vec3(uLight, 5.0, -2.0);
-      
-      vec3 rayDirection = normalize(vec3(screenCoords, -1.0));
-      vec3 rayOrigin = vec3(0.0, 0.1, 1);
-      Ray primaryRay = Ray(rayOrigin, rayDirection);
-
-      fragColor = shade(primaryRay);
-    }
-  `
+  
+    fragColor = vec4(finalColor, 1.0);
+  }`
 }
 
 /** @type {Canvas} */
@@ -394,9 +376,10 @@ class Inputs {
   }
 
   setupBounces () {
-    this.bounces = 1
+    this.bounces = 2
     document.getElementById('bounces').addEventListener('input', e => {
       this.bounces = e.target.value
+      scene.draw()
     })
   }
 }
